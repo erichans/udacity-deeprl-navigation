7e990d4c9826f3a57c19621c04e1fe8508473542
Eric Hans Silva <erichans@gmail.com>
2020-03-15 16:57:01 -0300

    refactored main

--- agent.py
+++ agent.py
@@ -1,5 +1,90 @@
-class Agent:
-    def __init__(self):
-        pass
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+
+import random
+import numpy as np
+
+from buffer import UniformReplayBuffer
+from model import OriginalDQN, DuelDQN
+
+LR = 5e-4
+BUFFER_SIZE = int(1e5)
+BATCH_SIZE = 64
+
+GAMMA = .99
+
+TAU = 1e-3
+UPDATE_EVERY = 4
+
+class DQNAgent:
+    def __init__(self, state_size, action_size, double_dqn, dueling_dqn, seed):
+        self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
         
+        self.double_dqn = double_dqn
+        self.dueling = dueling_dqn
+        
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(seed)
+        
+        if self.dueling:
+            self.qnetwork_local = DuelDQN(self.state_size, self.action_size, seed).to(self.device)
+            self.qnetwork_target = DuelDQN(self.state_size, self.action_size, seed).to(self.device)
+        else:
+            self.qnetwork_local = OriginalDQN(self.state_size, self.action_size, seed).to(self.device)
+            self.qnetwork_target = OriginalDQN(self.state_size, self.action_size, seed).to(self.device)
+            
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+        
+        self.replay_buffer = UniformReplayBuffer(self.action_size, BUFFER_SIZE, BATCH_SIZE, seed, self.device)
+        
+        self.t_step = 0
+        
+        print('Device used: {}'.format(self.device)) 
+        print('Double DQN?', self.double_dqn)
+        print('Dueling?', self.dueling)
+        print('Local DQN ->', self.qnetwork_local)
+        print('Target DQN ->', self.qnetwork_target)    
     
+    def act(self, state, epsilon=0.0):
+        state = torch.from_numpy(state).float().to(self.device)
+        
+        if random.random() > epsilon:
+            self.qnetwork_local.eval()
+            with torch.no_grad():
+                action = np.argmax(self.qnetwork_local(state).cpu().data.numpy())
+            self.qnetwork_local.train()
+        else:
+            action = np.random.randint(self.action_size)
+        
+        return int(action)
+    
+    def step(self, state, action, reward, next_state, done):
+        self.replay_buffer.add(state, action, reward, next_state, done)
+        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        if self.t_step == 0 and len(self.replay_buffer) >= BATCH_SIZE:
+            self._learn(self.replay_buffer.sample(), GAMMA)
+    
+    def _learn(self, experiences, gamma):
+        states, actions, rewards, next_states, dones = experiences
+        if self.double_dqn:
+            Q_action_targets_next = self.qnetwork_local(next_states).detach().argmax(1).unsqueeze(1)
+            Q_targets_next = self.qnetwork_target(next_states).gather(1, Q_action_targets_next)
+        else:
+            Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+            
+        Q_target = rewards + gamma * Q_targets_next * (1 - dones)
+        Q_expected = self.qnetwork_local(states).gather(1, actions)
+        
+        
+        self.optimizer.zero_grad()
+        loss = F.mse_loss(Q_expected, Q_target)
+        loss.backward()
+        self.optimizer.step()
+        
+        self._soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
+    
+    def _soft_update(self, local_model, target_model, tau):
+        for local_parameter, target_parameter in zip(local_model.parameters(), target_model.parameters()):
+            target_parameter.data.copy_((1.0-tau)*target_parameter+(tau*local_parameter))
---
+++ main.py
@@ -0,0 +1,117 @@
+from agent import DQNAgent
+
+from collections import deque
+
+from unityagents import UnityEnvironment
+
+import torch
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+def start_env():
+    env = UnityEnvironment(file_name="Banana_Windows_x86_64/banana.exe")
+
+    # get the default brain
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+
+    # reset the environment
+    env_info = env.reset(train_mode=True)[brain_name]
+
+    # number of agents in the environment
+    print('Number of agents:', len(env_info.agents))
+
+    # number of actions
+    action_size = brain.vector_action_space_size
+    print('Number of actions:', action_size)
+
+    # examine the state space 
+    state = env_info.vector_observations[0]
+    print('States look like:', state)
+    state_size = len(state)
+    print('States have length:', state_size)
+    
+    return env
+
+def get_brain_name(env):
+    return env.brain_names[0]
+    
+def get_brain(env):
+    return env.brains[get_brain_name(env)]
+    
+def get_state_size(env_info):
+    return len(env_info.vector_observations[0])
+    
+def get_action_size(env):
+    return get_brain(env).vector_action_space_size
+    
+def reset_env_info(env):
+    return env.reset(train_mode=True)[get_brain_name(env)]
+    
+def env_step(env, action):
+    return env.step(action)[get_brain_name(env)]
+
+
+def dqn_run(episodes=2500, eps_start=1.0, eps_end=0.01, eps_decay=0.995, double_dqn=False, dueling_dqn=False, seed=42):
+    env = start_env()
+    env_info = reset_env_info(env)
+    
+    state_size = get_state_size(env_info)
+    action_size = get_action_size(env)
+    
+    print('Seed used:', seed)
+    agent = DQNAgent(state_size, action_size, double_dqn, dueling_dqn, seed)
+    
+    scores = []
+    scores_window = deque(maxlen=100)
+    eps = eps_start
+
+    for episode in range(1, episodes+1):
+        env_info = reset_env_info(env)
+        score = 0.0
+        done = False
+        while not done:
+            state = env_info.vector_observations[0]
+            action = agent.act(state, epsilon=eps)
+            env_info = env_step(env, action)
+            next_state = env_info.vector_observations[0]
+            reward = env_info.rewards[0]
+            done = env_info.local_done[0]
+            agent.step(state, action, reward, next_state, done)
+            score += reward
+
+        scores_window.append(score)
+        scores.append(score)
+        eps = max(eps*eps_decay, eps_end)
+        print('\rEpisode {}/{}\tAverage Score: {:.2f}, epsilon: {:.3f}'.format(episode, episodes, np.mean(scores_window), eps), end='     ')
+        if episode % 100 == 0:
+            print('\rEpisode {}/{}\tAverage Score: {:.2f}, epsilon: {:.3f}'.format(episode, episodes, np.mean(scores_window), eps))
+        if np.mean(scores_window) > 13.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+    
+    env.close()
+    return scores
+    
+def save_scores(scores):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(len(scores)), scores)
+    plt.xlabel('Episode #')
+    plt.ylabel('Score')
+    plt.savefig('score-evolution.png')
+    #plt.show()
+
+TOTAL_EPISODES = 500
+EPSILON_START = 1.0
+EPSILON_DECAY = .99
+EPSILON_END = 0.01
+DOUBLE_DQN = True
+DUELING_DQN = True
+SEED = 42
+
+if __name__ == '__main__':    
+    scores = dqn_run(TOTAL_EPISODES, EPSILON_START, EPSILON_END, EPSILON_DECAY, DOUBLE_DQN, DUELING_DQN, SEED)
+    save_scores(scores)
